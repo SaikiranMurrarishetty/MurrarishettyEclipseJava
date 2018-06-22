b15e89c908ef26592b3aa9521d799188fb0a5071
Saikiran Murarishetty <saikiran.murarishetty@gmail.com>
2018-06-22 14:07:43 -0500

    Third commit

--- GitProjectDemo/src/FirstClass.java
+++ GitProjectDemo/src/FirstClass.java
@@ -1,9 +1,47 @@
-
+import java.util.*;
 public class FirstClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello World");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Input");
+		
+		int no = sc.nextInt();
+		System.out.print("<---------------------------------->\n"); 
+        for(int i = 0;i<no;i++)
+       {
+           for(int j = 0;j<=i;j++)
+           {
+                System.out.print("*");
+           }
+             System.out.print("\n");
+       }
+         
+        System.out.print("<---------------------------------->\n"); 
+         
+         
+      
+      for(int i = 1;i<=no;i++)
+       {
+           for(int m = i;m<no;m++)
+           {
+               
+               System.out.print(" ");
+           }
+           for(int j = 1;j<=(2*i-1);j++)
+           {
+                System.out.print("*");
+           }
+             System.out.print("\n");
+       }
+       
+      System.out.print("<---------------------------------->\n"); 
+       for(int d = 1;d<11;d++)
+       {
+           System.out.println(no+"*"+d+"="+(no*d));
+       }
+       
+		
 	}
 
 }
