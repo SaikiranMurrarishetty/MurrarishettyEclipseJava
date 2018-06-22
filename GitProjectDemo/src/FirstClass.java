1b6f23f040700a94bae91db3b171be78b46c5da1
Saikiran Murarishetty <saikiran.murarishetty@gmail.com>
2018-06-22 14:31:51 -0500

    Last Commit

--- GitProjectDemo/src/FirstClass.java
+++ GitProjectDemo/src/FirstClass.java
@@ -4,7 +4,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Input");
+		System.out.println("Enter Input:Number of lines\n");
 		
 		int no = sc.nextInt();
 		System.out.print("<---------------------------------->\n"); 
@@ -41,7 +41,29 @@ public class FirstClass {
            System.out.println(no+"*"+d+"="+(no*d));
        }
        
-		
+       System.out.print("<---------------------------------->\n"); 
+       
+       System.out.print("Enter String to be split at :\n");
+       String source = sc.next();
+       String[] split = source.split(":");
+       for(String s:split)
+       {
+    	   System.out.println(s); 
+       }
+       System.out.print("<---------------------------------->\n"); 
+       
+       System.out.print("Enter Source String with &(& will be replaced by %):\n");
+       source = sc.next();
+       String dest = source.replace('&', '%');
+       System.out.println("Final string with & replaced with %:"+dest); 
+       
+       System.out.print("<---------------------------------->\n"); 
+       System.out.print("Enter Source String\n");
+       source = sc.next();
+       String answer = source.substring(5, 10);
+       System.out.println("Substring from index 5 to 10:"+answer); 
+       
+       System.out.print("<---------------------------------->\n"); 
 	}
 
 }
