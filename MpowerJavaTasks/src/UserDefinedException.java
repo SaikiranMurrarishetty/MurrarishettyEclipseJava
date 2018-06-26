64067d6b8fd02f4b93c33debb40f7082958ef7e5
Saikiran Murarishetty <saikiran.murarishetty@gmail.com>
2018-06-26 08:47:06 -0500

    06-26-2018

---
+++ MpowerJavaTasks/src/UserDefinedException.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class UserDefinedException {
+
+	public static void main(String[] args) throws InvalidAmountException
+	{
+		// TODO Auto-generated method stub
+
+	System.out.println("Please Enter a Valid Amount(Positive Value from 1 to 100)\n");
+	int no = (new Scanner(System.in)).nextInt();
+	
+	if(no < 0 || no > 100)
+		throw new InvalidAmountException("Amount Value not in range of 1-100");
+	}
+
+}
